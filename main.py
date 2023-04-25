1db3564d6a94b6e8fcccb5f6e66fe512a4bfd9a2
tejasps2001 <tejasps2001@gmail.com>
2023-04-25 18:39:32 +0530

    Formatted code

--- main.py
+++ main.py
@@ -7,24 +7,11 @@ import subprocess
 
 # Global flag to indicate if download() is finished.
 download_finished = False
-    
-# Automatically download and import third party python modules.
-while ModuleNotFoundError:
-    try:
-        # Import third-party modules here.
-        import pyinputplus as pypi
-    except ModuleNotFoundError as m:
-        # Use \r because sometimes progressBar() finishes later
-        # than download() and there will be dots in newline.
-        print(f'\r{m.name} not found.') 
-        install(m.name)
-    else:
-        print('\rChecking for packages...Present')
-        break
 
 def main():
     task = initialise()
     # pypi.inputEmail('Enter your E-mail Address.')
+
 
 def initialise():
     """Create a welcome screen when the script is executed.
@@ -32,25 +19,25 @@ def initialise():
     Returns:
         _str_: accept user's choice to decide what the script should do.
     """
-    print('Initialising...')
+    print("Initialising...")
     print()
-    print('Welcome to System Monitor Alert.'.center(66, "-"))
-    print('------------------------------------------------------------------')
+    print("Welcome to System Monitor Alert.".center(66, "-"))
+    print("------------------------------------------------------------------")
     time.sleep(1)
     print("A continuously running python script that monitors your system's")
-    print('performance and sends alerts to your email if the system overloads.')
-    print('------------------------------------------------------------------')
+    print("performance and sends alerts to your email if the system overloads.")
+    print("------------------------------------------------------------------")
     time.sleep(1)
     print()
-    print('WHAT DO YOU WANT ME TO DO?'.center(66))
+    print("WHAT DO YOU WANT ME TO DO?".center(66))
     print()
-    print('1. Show status of your system, or;')
-    print('2. Start monitoring')
+    print("1. Show status of your system, or;")
+    print("2. Start monitoring")
     time.sleep(1)
     print()
-    task = pypi.inputChoice(['1', '2'])
+    task = pypi.inputChoice(["1", "2"])
     return task
-    
+
 
 def install(moduleName):
     """Call the download() and progressBar to install the third party
@@ -64,8 +51,9 @@ def install(moduleName):
     installObj = threading.Thread(target=download, args=[moduleName])
     installObj.start()
     while not download_finished:
-        progressBar('Downloading')
+        progressBar("Downloading")
     installObj.join()
+
 
 def download(moduleName):
     """Download the necessary third-party modules from the internet.
@@ -74,14 +62,15 @@ def download(moduleName):
         moduleName (_str_): module to download.
     """
     global download_finished
-    install_cmd = f'pip install {moduleName} -q'
+    install_cmd = f"pip install {moduleName} -q"
     install = subprocess.run(install_cmd, shell=True)
     if install.returncode == 0:
-        print(f'\nInstalled {moduleName}.')
+        print(f"\nInstalled {moduleName}.")
     download_finished = True
 
+
 def progressBar(text):
-    """Run a nice progress indicator when a program is executing 
+    """Run a nice progress indicator when a program is executing
        in another thread.
 
     Args:
@@ -89,18 +78,32 @@ def progressBar(text):
     """
     # Hide cursor bar.
     global download_finished
-    print('\033[?25l', end="")
+    print("\033[?25l", end="")
     # \r to go back to beginning of the line.
     # \033[K to clear the text from the current position to the EOL.
-    print('\r\033[K', end='')
-    print(text, end='')
+    print("\r\033[K", end="")
+    print(text, end="")
     for i in range(3):
         if download_finished == True:
             break
-        print('.', end='', flush=True)
+        print(".", end="", flush=True)
         time.sleep(1)
     # Show the cursor again.
-    print('\033[?025h', end='')
+    print("\033[?025h", end="")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    # Automatically download and import third party python modules.
+    while ModuleNotFoundError:
+        try:
+            # Import third-party modules here.
+            import pyinputplus as pypi
+        except ModuleNotFoundError as m:
+            # Use \r because sometimes progressBar() finishes later
+            # than download() and there will be dots in newline.
+            print(f"\r{m.name} not found.")
+            install(m.name)
+        else:
+            print("\rChecking for packages...Present")
+            break
     main()
